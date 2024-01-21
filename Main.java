94e3ec13b51ed52e1b6baa2b69d3e78812b56b3d
KairiHer <144934520+KairiHer@users.noreply.github.com>
2024-01-20 18:21:30 -0800

    Add files via upload
    This is the Hello World Java Code.

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        int num1 = 2;
+        int num2 = 4;
+        int sum = 0;
+
+        sum = num1 + num2;
+
+        System.out.println("\n\n The sum of " + num1 + " and " + num2 + " is: " + sum + "\n\n");
+    }
+}
